e00acae2d398c58393c632aae9603cfd482f8c1a
UMESH BADSARA <umesh2505badsara@gmail.com>
2026-02-24 18:01:50 +0530

    playing with query_sets

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render 
-from store.models import Order
+from django.db.models import Count
+from store.models import Order ,Customer
 
 
 
@@ -9,5 +10,9 @@ def hello(request):
         'first_name': 'Umesh',
         'last_name': 'Badsara',
     }
-    orders = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
-    return render(request, 'hello.html', {'details': details , 'orders':orders})
+    queryset = Customer.objects.annotate(
+        orders_count = Count('order')
+    )
+    # orders = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    return render(request, 'hello.html', {'details': details ,'queryset': queryset})
+ 
