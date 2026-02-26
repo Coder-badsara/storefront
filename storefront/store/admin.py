7f09d31e5cecac93a2c766b6f35ab9429133667b
UMESH BADSARA <umesh2505badsara@gmail.com>
2026-02-26 14:26:36 +0530

    added new app Store_custom

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,29 +1,61 @@
-from django.contrib import admin
+from django.contrib import admin , messages
 from django.urls import reverse 
+from django.db.models.query import QuerySet
 from django.utils.html import format_html , urlencode
 from django.db.models import Count
 from . import models
 
 # Register your models here.
+
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+    
+    def lookups(self, request, model_admin):
+        return [
+            ('<100', 'Low'),
+        ]
+    def queryset(self, request, queryset):
+        if self.value() == '<100':
+            return queryset.filter(inventory__lt=100)
+        
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price', 'inventory_status', 'collection']
+    exclude = ['promotions']
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+    actions = ['clear_inventory']
+    list_display = ['title', 'price', 'inventory_status', 'collection_title']
     list_editable = ['price']
-    list_filter = ['collection', 'last_updated']
+    list_filter = ['collection', 'last_updated', InventoryFilter]
     list_per_page = 10
+    list_select_related = ['collection']
+    search_fields = ['title__istartswith']
+    
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 100:
             return 'Low'
         return 'OK'
+    
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(request, f'{updated_count} products were successfully updated.', messages.SUCCESS)
+        messages.ERROR
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'order_count']
     list_editable = ['membership']
     list_per_page = 10
-    list_ordering = ['first_name', 'last_name']
+    ordering = ['first_name', 'last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
     
     
@@ -39,15 +71,23 @@ class CustomerAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(order_count = Count('order'))
 
+
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id','customer', 'placed_at', 'payment_status']
-    ordering = ['-placed_at']
-    list_per_page = 20
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+    list_display = ['id', 'placed_at', 'customer']
     
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'product_count']
+    search_fields = ['title']
     
     @admin.display(ordering='product_count')
     def product_count(self, collection):
--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 class Promotion(models.Model):
@@ -9,21 +10,34 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
     
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ['title']
+        
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    inventory = models.IntegerField()
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(0)]
+        )
+    inventory = models.IntegerField( validators=[MinValueValidator(0)])
     last_updated = models.DateTimeField(auto_now_add=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
     
+    
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ['title']
     
 class Customer(models.Model):
     
