5b0056d6ccf81cbdbe23e1c5a9eee546911e233e
UMESH BADSARA <umesh2505badsara@gmail.com>
2026-03-25 13:57:51 +0530

    serializers/deserializers

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -2,13 +2,23 @@ from decimal import Decimal
 from .models import Product , Collection
 from rest_framework import serializers
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length = 255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2)
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'price', 'price_with_tax', 'inventory', 'collection']
+
+    # --> this will duplicate the variables i.e also declared here and also in models
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length = 255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2)
     price_with_tax = serializers.SerializerMethodField(method_name= 'calculate_tax')
-    collection = serializers.StringRelatedField()
+    # collection = serializers.StringRelatedField()
     
     def calculate_tax(self, product: Product):
         final_value = product.price * Decimal(1.1)
         return final_value
+    
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']     
--- storefront/store/views.py
+++ storefront/store/views.py
@@ -6,16 +6,30 @@ from .models import Product
 from .serializers import ProductSerializer
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    product_list = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(product_list, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        product_list = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(product_list, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view()
+
+@api_view(['GET', 'PUT'])
 def products_detail(request, id):
     product = get_object_or_404(Product, pk=id)
-    serializer = ProductSerializer(product) 
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product) 
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+        
 
         
